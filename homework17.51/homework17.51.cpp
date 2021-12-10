feabab7d72c145fc98e29868f7d680d3dd82df85
Иван <wanjakulie1v1999@mail.ru>
2021-12-10 13:39:42 +0300

    ready

---
+++ homework17.51/homework17.51.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <vector>
+
+class Vector
+{
+private:
+	double x;
+	double y;
+	double z;
+public:
+	double GetX()
+	{
+		return x;
+	}
+	double GetY()
+	{
+		return y;
+	}
+	double GetZ()
+	{
+		return z;
+	}
+	void SetX(double newX)
+	{
+		x = newX;
+	}
+	void SetY(double newY)
+	{
+		y = newY;
+	}
+	void SetZ(double newZ)
+	{
+		z = newZ;
+	}
+	double GetScale()
+	{
+		double Scale = x + y + z;
+		return Scale;
+	}
+};
+
+class example
+{
+private:
+	int a = 24;
+
+public:
+	int GetA()
+	{
+		return a;
+	}
+};
+
+int main()
+{
+	example temp;
+	temp.GetA();
+	std::cout << temp.GetA() << "\n";
+	Vector massive;
+	massive.GetX();
+	massive.GetY();
+	massive.GetZ();
+	massive.SetX(-15);
+	massive.SetY(-12);
+	massive.SetZ(-21);
+	massive.GetScale();
+	std::cout << abs(massive.GetScale()) << "\n";
+	std::vector<double>array{ massive.GetX(), massive.GetY(), massive.GetZ() };
+	std::cout << array.size();
+
+}
